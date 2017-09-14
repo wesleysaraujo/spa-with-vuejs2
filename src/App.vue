586f812d06be76682815950d49293b6ff8412d59
Wesley S. Araújo <wsadesigner@gmail.com>
2017-09-14 01:33:36 -0300

    Adicionando o bootswatch à aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="container">
+      <img src="./assets/logo.png">
+      <router-view></router-view>
+      <button type="button" name="button" class="btn btn-primary">Olá mundo</button>
+    </div>
+
   </div>
 </template>
 
